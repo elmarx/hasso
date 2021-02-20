55347b443ee40407955d5de8f3df18e3de385e87
Elmar Athmer <elmar@athmer.org>
2021-02-20 22:43:33 +0100

    import all (currently) known events/domains

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,39 @@ export type Context = {
   user_id: string | null;
 };
 
+// list of domains generated via `curl -s -H "Authorization: Bearer $HASS_TOKEN" $HASS_URL/api/services | jq .[].domain | sort | uniq`
+export type Domain =
+  | "automation"
+  | "cast"
+  | "cloud"
+  | "counter"
+  | "device_tracker"
+  | "frontend"
+  | "group"
+  | "homeassistant"
+  | "input_boolean"
+  | "input_datetime"
+  | "input_number"
+  | "input_select"
+  | "input_text"
+  | "kodi"
+  | "light"
+  | "logbook"
+  | "media_player"
+  | "mqtt"
+  | "notify"
+  | "persistent_notification"
+  | "person"
+  | "recorder"
+  | "scene"
+  | "script"
+  | "stream"
+  | "switch"
+  | "system_log"
+  | "timer"
+  | "tts"
+  | "zone";
+
 /** actual api responses **/
 
 export type Config = {
--- src/api/events.ts
+++ src/api/events.ts
@@ -1,7 +1,19 @@
 import { Context } from "./api";
 
+// events as returned via `curl -s -H "Authorization: Bearer $HASS_TOKEN" $HASS_URL/api/events | jq .[].event | sort | uniq`
+// then replace `^"(.*)"$` with `\U$1\E = "$1",`
 export const enum Event {
+  COMPONENT_LOADED = "component_loaded",
+  CORE_CONFIG_UPDATED = "core_config_updated",
+  DEVICE_REGISTRY_UPDATED = "device_registry_updated",
+  ENTITY_REGISTRY_UPDATED = "entity_registry_updated",
+  HOMEASSISTANT_CLOSE = "homeassistant_close",
+  HOMEASSISTANT_START = "homeassistant_start",
+  HOMEASSISTANT_STOP = "homeassistant_stop",
+  PLATFORM_DISCOVERED = "platform_discovered",
+  STATE_CHANGED = "state_changed",
   TAG_SCANNED = "tag_scanned",
+  USER_REMOVED = "user_removed",
 }
 
 export type TagScannedEvent = {
