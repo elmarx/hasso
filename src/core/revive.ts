8cf4f76fecbe99b19561207e739bd0920979e2b3
Elmar Athmer <elmar@athmer.org>
2021-03-08 19:20:26 +0100

    revive dates in StateChangedEvents

--- src/core/revive.ts
+++ src/core/revive.ts
@@ -1,11 +1,26 @@
-import { RawState } from "../model/raw";
-import { HassEvent, State } from "../model";
+import { RawState, RawStateChanged } from "../model/raw";
+import { HassEvent, State, StateChangedEvent } from "../model";
 
 export function reviveState(s: RawState): State {
   return {
     ...s,
     last_changed: new Date(s.last_changed),
     last_updated: new Date(s.last_updated),
+  };
+}
+
+export function reviveStateChanged(s: RawStateChanged): StateChangedEvent {
+  const oldState = s.data.old_state ? reviveState(s.data.old_state) : null;
+  const newState = s.data.new_state ? reviveState(s.data.new_state) : null;
+
+  return {
+    ...s,
+    data: {
+      ...s.data,
+      old_state: oldState,
+      new_state: newState,
+    },
+    time_fired: new Date(s.time_fired),
   };
 }
 
@@ -25,6 +40,8 @@ export function reviveEvent<T extends { time_fired: string }>(
   event: T
 ) {
   switch (eventName) {
+    case HassEvent.STATE_CHANGED:
+      return reviveStateChanged(event as any);
     default:
       return reviveTimeFired(event);
   }
